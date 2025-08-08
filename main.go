de457b7e59f471f1f76b34d089b84b89c3d3f738
Thijs-Desjardijn <thijsproo@gmail.com>
2025-08-08 13:58:14 +0200

    added saving and finding pokemon

--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+
+	//"log"
+	"encoding/gob"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/Thijs-Desjardijn/pokedex/pokecache"
-
-	"io"
-	"net/http"
-
-	//"net/url"
-	"math/rand"
-	"os"
-	"strings"
-	//"errors"
 )
 
 type Config struct {
@@ -73,15 +73,50 @@ type TypeInfo struct {
 	Name string `json:"name"`
 }
 
+var catchablePokemon map[string]PokemonInformation
+var PokeDex map[string]PokemonInformation
+var cache *pokecache.Cache
+var supportedCommands map[string]cliCommand
+
+func commandFind(_ *Config, area string) error {
+	fmt.Printf("Looking for pokemon at %s\n", area)
+	url := "https://pokeapi.co/api/v2/location-area/" + area + "/"
+	data, err := GetData(cache, url)
+	if err != nil {
+		return err
+	}
+	var areaInfo LocationArea
+	err = json.Unmarshal(data, &areaInfo)
+	if err != nil {
+		return err
+	}
+	index := rand.Intn(len(areaInfo.PokemonEncounters))
+	pokemonName := areaInfo.PokemonEncounters[index].Pokemon.Name
+	url = "https://pokeapi.co/api/v2/pokemon/" + pokemonName
+	data1, err := GetData(cache, url)
+	if err != nil {
+		return err
+	}
+
+	var pokemon PokemonInformation
+	err = json.Unmarshal(data1, &pokemon)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("You found a %s!\nYou are now able to catch it using the catch command\n", pokemonName)
+	catchablePokemon[pokemonName] = pokemon
+	return nil
+}
+
 func commandPokedex(_ *Config, _ string) error {
 	fmt.Println("Your Pokedex:")
-	for _, pokemon := range pokeDex {
+	for _, pokemon := range PokeDex {
 		fmt.Printf("- %s\n", pokemon.Name)
 	}
 	return nil
 }
 func commandInspect(_ *Config, pokemonName string) error {
-	pokemon, ok := pokeDex[pokemonName]
+	pokemon, ok := PokeDex[pokemonName]
 	if !ok {
 		fmt.Println("You have not yet caught this pokemon")
 		return nil
@@ -105,6 +140,10 @@ func cleanInput(text string) []string {
 }
 
 func commandExit(cfg *Config, _ string) error {
+	err := commandSave(&Config{}, "")
+	if err != nil {
+		return err
+	}
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
@@ -169,9 +208,9 @@ func commandMapb(cfg *Config, _ string) error {
 }
 
 func GetData(c *pokecache.Cache, url string) ([]byte, error) {
+	//function for all https requests automatically adds the info to the cache
 	data, ok := c.Get(url)
 	if ok {
-		fmt.Println("Cache used!")
 		return data, nil
 	} else {
 		res, err := http.Get(url)
@@ -184,7 +223,6 @@ func GetData(c *pokecache.Cache, url string) ([]byte, error) {
 		}
 		body, err := io.ReadAll(res.Body)
 		if err != nil {
-			fmt.Println(err)
 			return []byte{}, err
 		}
 		c.Add(url, body)
@@ -192,21 +230,15 @@ func GetData(c *pokecache.Cache, url string) ([]byte, error) {
 	}
 }
 
-var pokeDex map[string]PokemonInformation
-var cache *pokecache.Cache
-var supportedCommands map[string]cliCommand
-
 func commandExplore(_ *Config, nameLocation string) error {
 	url := "https://pokeapi.co/api/v2/location-area/" + nameLocation + "/"
 	data, err := GetData(cache, url)
 	if err != nil {
-		fmt.Printf("1:%v\n", err)
 		return err
 	}
 	var area LocationArea
 	err = json.Unmarshal(data, &area)
 	if err != nil {
-		fmt.Printf("2:%v\n", err)
 		return err
 	}
 	for _, pokemon := range area.PokemonEncounters {
@@ -216,17 +248,10 @@ func commandExplore(_ *Config, nameLocation string) error {
 }
 
 func commandCatch(_ *Config, pokemonName string) error {
-	url := "https://pokeapi.co/api/v2/pokemon/" + pokemonName
-	data, err := GetData(cache, url)
-	if err != nil {
-		fmt.Println("1", err)
-		return err
-	}
-	var pokemon PokemonInformation
-	err = json.Unmarshal(data, &pokemon)
-	if err != nil {
-		fmt.Println("2", err)
-		return err
+	pokemon, ok := catchablePokemon[pokemonName]
+	if !ok {
+		fmt.Printf("You have not yet found %s", pokemonName)
+		return nil
 	}
 	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
 	const (
@@ -249,18 +274,67 @@ func commandCatch(_ *Config, pokemonName string) error {
 		fmt.Printf("%s escaped!\n", pokemonName)
 		return nil
 	}
-	pokeDex[pokemonName] = pokemon
+	PokeDex[pokemonName] = pokemon
 	return nil
 }
 
+func commandSave(_ *Config, _ string) error {
+	fmt.Printf("Saving your progress\nDo not shut off the program\n")
+	filename := "save_" + time.Now().Format("20060102_150405") + ".bin"
+	file, err := os.Create("save_folder/" + filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(PokeDex)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Save sucessful")
+	return nil
+}
+
+func readSave() error {
+	files, err := os.ReadDir("save_folder")
+	if err != nil {
+		return err
+	}
+	var mostRecentFile string
+	var mostRecentTime time.Time
+	for _, file := range files {
+		name := file.Name()
+		time_s := name[5:20]
+		t, err := time.Parse("20060102_150405", time_s)
+		if err != nil {
+			return err
+		}
+		if t.After(mostRecentTime) {
+			mostRecentTime = t
+			mostRecentFile = name
+		}
+	}
+	fullpath := filepath.Join("save_folder", mostRecentFile)
+	file, err := os.Open(fullpath)
+	if err != nil {
+		return err
+	}
+	gob.NewDecoder(file).Decode(&PokeDex)
+	return nil
+}
 func main() {
 	cfg := &Config{}
-	cache = pokecache.NewCache(60 * time.Second)
-	pokeDex = make(map[string]PokemonInformation)
+	cache = pokecache.NewCache(120 * time.Second)
+	PokeDex = make(map[string]PokemonInformation)
+	err := readSave()
+	if err != nil {
+		fmt.Printf("Unable to load save: %v\nPlease try again\n", err)
+	}
+	catchablePokemon = make(map[string]PokemonInformation)
 	supportedCommands = map[string]cliCommand{
 		"exit": {
 			name:        "exit",
-			description: "Exit the Pokedex",
+			description: "Exit the Pokedex and save your progress",
 			callback:    commandExit,
 		},
 
@@ -283,7 +357,7 @@ func main() {
 		},
 
 		"explore": {
-			name:        "commandexplore",
+			name:        "explore",
 			description: "Displays the pokemons that can be found in a specified location after the command",
 			callback:    commandExplore,
 		},
@@ -304,6 +378,18 @@ func main() {
 			name:        "pokedex",
 			description: "Displays all your caught pokemons in your Pokedex",
 			callback:    commandPokedex,
+		},
+
+		"find": {
+			name:        "find",
+			description: "Use this command to find pokemon in an area",
+			callback:    commandFind,
+		},
+
+		"save": {
+			name:        "save",
+			description: "Command to save you progress",
+			callback:    commandSave,
 		},
 	}
 	scanner := bufio.NewScanner(os.Stdin)
@@ -326,7 +412,10 @@ func main() {
 				secondCommand = cleanedInput[1]
 			}
 			if cmd, exists := supportedCommands[command]; exists {
-				cmd.callback(cfg, secondCommand)
+				err := cmd.callback(cfg, secondCommand)
+				if err != nil {
+					fmt.Println(err)
+				}
 			} else {
 				fmt.Println("Unknown command")
 			}
